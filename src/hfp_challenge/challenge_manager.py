3f463d59d2dab75a3518309d1fdfaf5efb0cd272
aliyuldashev <javohir.hbai@gmail.com>
2026-04-01 16:47:38 +0900

    fix: update min_score to improve challenge scoring logic

--- src/hfp_challenge/challenge_manager.py
+++ src/hfp_challenge/challenge_manager.py
@@ -29,7 +29,7 @@ class HFPChallengeManager(ChallengeManager):
 
         self.max_similarity = 0.4
         self.min_similarity = 0
-        self.min_score = 0.1
+        self.min_score = 0.4
         self.break_point = 0.6
         self.max_input = 1.0
         self.min_value = 0
